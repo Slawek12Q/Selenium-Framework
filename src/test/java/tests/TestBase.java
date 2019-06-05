6b6e902da6b53ebd9e89f8f0bf85f0d2f1ba3d71
Slawek12Q <slawek.guzik@interia.pl>
2019-06-06 00:10:49 +0200

    create test base

---
+++ src/test/java/tests/TestBase.java
@@ -0,0 +1,30 @@
+package tests;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+
+import java.util.concurrent.TimeUnit;
+
+public class TestBase {
+    private WebDriver driver;
+
+    @BeforeMethod
+    public void beforeTest(){
+        System.setProperty("webdriver.chrome.driver", "C:\\driver\\chromedriver.exe");
+
+        driver = new ChromeDriver();
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        driver.manage().window().maximize();
+        driver.navigate().to("www.google.pl");
+    }
+
+
+    @AfterMethod
+    public void afterTest(){
+        driver.close();
+        driver.quit();
+        driver = null;
+    }
+}
